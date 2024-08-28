2b7453eb370163805e732d8037e0789ed4eb8354
harold <haroldulrich@protonmail.com>
2024-08-27 19:33:49 -0700

    scaling multi_var_logistic_reg_model

--- models/multi_var_logistic_reg_model.py
+++ models/multi_var_logistic_reg_model.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -20,14 +21,27 @@ sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
 plt.title('Pearson Correlation Matrix')
 plt.show()
 
+# Split Test and Train sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Scale X sets
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
 # Train logistic regression model
-model = LogisticRegression(random_state=42)
-model.fit(X_train, y_train)
+# Solvers: 'liblinear', 'lbfgs', or 'saga'
+model = LogisticRegression(
+    random_state=42, 
+    max_iter=300, 
+    class_weight='balanced',
+    solver='saga',
+    verbose=1
+)
+model.fit(X_train_scaled, y_train)
 
 # Make predictions
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test_scaled)
 
 # Evaluate the model
 print("Classification Report:")
